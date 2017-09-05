15a3867a3edb18bb8052d4ed1ebf830740ab0eb4
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2017-09-05 12:51:47 -0300

    serverjs

---
+++ server.js
@@ -0,0 +1,10 @@
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+console.log('App working in port ' + process.env.PORT || '8080');
