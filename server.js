1a739dc537d0d05934767f6243d0589b3bc7f2f5
Gerardo Grimaldi <grimaldi.gerardo@gmail.com>
2017-09-11 12:52:45 -0300

    HOTFIX contact

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
-console.log('App working in port ' + process.env.PORT || '8080');
+console.log('App working in port ' + (process.env.PORT || '8080'));
